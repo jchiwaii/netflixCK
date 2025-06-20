7b7b4b264521ef3b3711792cf604f25262e06894
Chiwai <chiwai.kiriba@gmail.com>
2025-06-20 16:30:16 +0300

    api's update

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,9 +1,13 @@
+// Check if API key exists
+if (!import.meta.env.VITE_TMDB_API_KEY) {
+  throw new Error("TMDB API key not found in environment variables");
+}
+
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODE1NjcxYzQ5NTJhNDIzNTRiNmQ1Y2Y5MTY0MDA2YSIsIm5iZiI6MTc1MDQxOTc0Mi41NjMsInN1YiI6IjY4NTU0OTFlNjg4YmU1NjVhYzVkOGQ2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U19mc6Em-1hGcn72U4UssD6s4ron2BFusuSPYRPOdaA",
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
   },
 };
 
